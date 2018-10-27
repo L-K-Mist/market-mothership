035e507952d5bbf24d32d812cd8e24d2b4dee96c
Dylan <dylan@vandenbosch.co.za>
2018-10-27 06:06:54 +0200

    coming along nicely

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
     </v-navigation-drawer>
     <v-toolbar color="indigo" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>The Market-Mother-Ship</v-toolbar-title>
     </v-toolbar>
     <v-content>
             <v-slide-y-transition mode="out-in">
